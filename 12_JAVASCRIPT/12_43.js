0b4c3f2bb763b8e397b5d8701c78af88900519aa
kyeongwxx <kyeongwxx@gmail.com>
2022-04-21 03:15:32 +0900

    12.7 다양한 함수의 형태

---
+++ 12_JAVASCRIPT/12_43.js
@@ -0,0 +1,7 @@
+function countdown(n) {
+    for (var i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countdown(10);
---
+++ 12_JAVASCRIPT/12_47.js
@@ -0,0 +1,16 @@
+function factorial(n) {
+    if(n <= 1) return 1;
+
+    var res = n;
+    while(--n) {
+        res = res * n;
+    }
+    return res;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
