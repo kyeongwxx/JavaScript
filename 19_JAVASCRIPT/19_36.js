becb668aaf877109734bc09694e6836a2fce3ac7
kyeongwxx <kyeongwxx@gmail.com>
2022-04-25 16:14:13 +0900

    19.8 오버라이딩과 프로퍼티 섀도잉

---
+++ 19_JAVASCRIPT/19_36.js
@@ -0,0 +1,24 @@
+const Person = (function (){
+    // 생성자 함수
+    function Person(name) {
+        this.name = name;
+    }
+
+    // 프로토타입 메소드
+    Person.prototype.sayHello = function() {
+        console.log(`Hi! My name is ${this.name}`);
+    };
+
+    // 생성자 함수를 반환
+    return Person;
+}());
+
+const me = new Person('Lee');
+
+// 인스턴스 메소드
+me.sayHello = function() {
+    console.log(`Hey! My name is ${this.name}`);
+};
+
+// 인스턴스 메소드가 호출된다. 프로토타입 메소드는 인스턴스 메소드에 의해 가려진다.
+me.sayHello();
---
+++ 19_JAVASCRIPT/19_39.js
@@ -0,0 +1,39 @@
+const Person = (function (){
+    // 생성자 함수
+    function Person(name) {
+        this.name = name;
+    }
+
+    // 프로토타입 메소드
+    Person.prototype.sayHello = function() {
+        console.log(`Hi! My name is ${this.name}`);
+    };
+
+    // 생성자 함수를 반환
+    return Person;
+}());
+
+const me = new Person('Lee');
+
+// 인스턴스 메소드
+me.sayHello = function() {
+    console.log(`Hey! My name is ${this.name}`);
+};
+
+// 인스턴스 메소드가 호출된다. 프로토타입 메소드는 인스턴스 메소드에 의해 가려진다.
+me.sayHello();
+
+// 인스턴스 메소드를 삭제한다.
+delete me.sayHello;
+// 인스턴스에는 sayHello 메소드가 없으므로 프로토타입 메소드가 호출된다.
+me.sayHello();
+
+// 프로토타입 메소드 변경
+Person.prototype.sayHello = function() {
+    console.log(`Hey! My name is ${this.name}`);
+};
+me.sayHello();
+
+// 프로토타입 메소드 삭제
+delete Person.prototype.sayHello;
+me.sayHello(); // TypeError: me.sayHello is not a function
