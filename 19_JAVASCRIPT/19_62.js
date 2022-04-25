5a8ee8fa8a31051906c659d3b4f1f7086d338f08
kyeongwxx <kyeongwxx@gmail.com>
2022-04-25 18:29:51 +0900

    19.13 프로퍼티 존재 확인

---
+++ 19_JAVASCRIPT/19_62.js
@@ -0,0 +1,4 @@
+const person = { name: 'Lee' };
+
+console.log(person.hasOwnProperty('name'));
+console.log(person.hasOwnProperty('age'));
---
+++ 19_JAVASCRIPT/19_63.js
@@ -0,0 +1,6 @@
+const person = { name: 'Lee' };
+
+console.log(person.hasOwnProperty('name'));
+console.log(person.hasOwnProperty('age'));
+
+console.log(person.hasOwnProperty('toString'));
