774d305bd1fb6e154da9c717ea1cee8f86508e46
kyeongwxx <kyeongwxx@gmail.com>
2022-04-25 21:55:32 +0900

    19.14 프로퍼티 열거

---
+++ 19_JAVASCRIPT/19_64.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Lee',
+    address: 'Seoul'
+};
+
+// for...in문의 변수 prop에 person 객체의 프로퍼티 키가 할당된다.
+for(const key in person) {
+    console.log(key + ': ' + person[key]);
+}
