7baba8fa2cbb34726cc8360b6921cb480efc202b
kyeongwxx <kyeongwxx@gmail.com>
2022-05-01 19:57:37 +0900

    27.2 자바스크립트 배열은 배열이 아니다

---
+++ 27_JAVASCRIPT/27_09.js
@@ -0,0 +1,9 @@
+console.log(Object.getOwnPropertyDescriptors([1, 2, 3]));
+/*
+{
+  '0': { value: 1, writable: true, enumerable: true, configurable: true },
+  '1': { value: 2, writable: true, enumerable: true, configurable: true },
+  '2': { value: 3, writable: true, enumerable: true, configurable: true },
+  length: { value: 3, writable: true, enumerable: false, configurable: false }
+}
+*/
