15987903c44ca6d99da1ef7d5f0f812dc0df4c7b
kyeongwxx <kyeongwxx@gmail.com>
2022-05-02 17:39:44 +0900

    27.6 배열 요소의 추가와 갱신

---
+++ 27_JAVASCRIPT/27_35.js
@@ -0,0 +1,12 @@
+const arr = [0];
+
+// 배열 요소의 추가
+arr[1] = 1;
+
+console.log(arr);
+console.log(arr.length);
+
+arr[100] = 100;
+
+console.log(arr);
+console.log(arr.length);
---
+++ 27_JAVASCRIPT/27_37.js
@@ -0,0 +1,33 @@
+const arr = [0];
+
+// 배열 요소의 추가
+arr[1] = 1;
+
+console.log(arr);
+console.log(arr.length);
+
+arr[100] = 100;
+
+console.log(arr);
+console.log(arr.length);
+
+// 명시적으로 값을 할당하지 않은 요소는 생성되지 않는다.
+console.log(Object.getOwnPropertyDescriptors(arr));
+/*
+{
+  '0': { value: 0, writable: true, enumerable: true, configurable: true },
+  '1': { value: 1, writable: true, enumerable: true, configurable: true },
+  '100': { value: 100, writable: true, enumerable: true, configurable: true },
+  length: {
+    value: 101,
+    writable: true,
+    enumerable: false,
+    configurable: false
+  }
+}
+*/
+
+// 요소값의 갱신
+arr[1] = 10;
+
+console.log(arr);
