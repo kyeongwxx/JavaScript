41578830827921b83b4d138c523800198489da2d
kyeongwxx <kyeongwxx@gmail.com>
2022-05-02 16:25:16 +0900

    27.3 length 프로퍼티와 희소 배열

---
+++ 27_JAVASCRIPT/27_12.js
@@ -0,0 +1,2 @@
+[].length
+[1, 2, 3].length
---
+++ 27_JAVASCRIPT/27_16.js
@@ -0,0 +1,16 @@
+const arr = [1];
+
+// 현재 length 프로퍼티 값인 1보다 큰 숫자 값 3을 length 프로퍼티에 할당
+arr.length =3;
+
+// length 프로퍼티 값은 변경되지만 실제로 배열의 길이가 늘어나지는 않는다.
+console.log(arr.length);
+console.log(arr);
+
+console.log(Object.getOwnPropertyDescriptors(arr));
+/*
+{
+  '0': { value: 1, writable: true, enumerable: true, configurable: true },
+  length: { value: 3, writable: true, enumerable: false, configurable: false }
+}
+*/
